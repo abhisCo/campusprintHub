01accb97af0624481729e8d882e6c8379c9072de
abhisCo <asjungade21gmail@gmail.com>
2026-06-16 15:26:18 +0530

    Day 1 Setup Complete

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <div className="p-10 text-4xl font-bold">
+      Welcome to CampusPrint Hub
+    </div>
+  )
+}
+
+export default Home
---
+++ frontend/src/pages/Signup.jsx
@@ -0,0 +1,9 @@
+const Signup = () => {
+  return (
+    <div className="p-10 text-3xl">
+      Signup Page
+    </div>
+  )
+}
+
+export default Signup
